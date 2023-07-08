0465b1d90674a2c678c2a5dee751e806e2aca2d8
pavanyendluri588 <pavanyendluri588@gmail.com>
2023-07-08 16:21:24 +0530

    pytorch upload

---
+++ touch.sh
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/pavanyendluri588/pytorch.git
+git remote -v  --force
+git add . --force
+git commit -m "pytorch upload"
+
+
+git push -f origin main
